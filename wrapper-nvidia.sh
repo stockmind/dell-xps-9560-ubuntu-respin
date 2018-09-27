2c17c990f31e8e97af126270fec9156b6d54d135
Matteo Iervasi <matteoiervasi@gmail.com>
2018-09-27 21:22:28 +0200

    Update wrapper-nvidia.sh

--- wrapper-nvidia.sh
+++ wrapper-nvidia.sh
@@ -4,9 +4,6 @@ cd /usr/local/bin
 
 sudo prime-select intel 2> /dev/null
 
-sudo mv gpuoff.service /lib/systemd/system/gpuoff.service
-sudo systemctl enable gpuoff
-
 echo "options i915 enable_fbc=1 enable_psr=2 enable_guc=-1 disable_power_well=0 fastboot=1" > /etc/modprobe.d/i915.conf
 update-initramfs -u
 
