7486bc9b3bbf3aca26bd8ab48b39788a7f9392aa
Matteo Iervasi <matteoiervasi@gmail.com>
2018-11-16 21:26:33 +0100

    Update wrapper-nvidia.sh

--- wrapper-nvidia.sh
+++ wrapper-nvidia.sh
@@ -6,6 +6,7 @@ sudo prime-select intel 2> /dev/null
 
 echo "options i915 enable_fbc=1 enable_psr=2 enable_guc=-1 disable_power_well=0 fastboot=1" > /etc/modprobe.d/i915.conf
 echo "options dell-smm-hwmon restricted=0 force=1" > /etc/modprobe.d/dell-smm-hwmon.conf
+echo "dell-smm-hwmon" > /etc/modules
 update-initramfs -u
 
 sudo rm -f /usr/local/bin/wrapper-nvidia.sh
