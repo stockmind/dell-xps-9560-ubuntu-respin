35d5cf1b13eb2707180874ab4f313c80001f3477
Matteo Iervasi <matteoiervasi@gmail.com>
2019-03-23 12:01:56 +0100

    Update xps-tweaks.sh

--- xps-tweaks.sh
+++ xps-tweaks.sh
@@ -146,7 +146,7 @@ apt-get install libavcodec-dev libldac pulseaudio-module-bluetooth
 apt -y install intel-microcode
 
 # Install wifi drivers
-rm -f /lib/firmware/ath10k/QCA6174/hw3.0/*
+rm -rf /lib/firmware/ath10k/QCA6174/hw3.0/*
 wget -O /lib/firmware/ath10k/QCA6174/hw3.0/board.bin https://github.com/kvalo/ath10k-firmware/blob/master/QCA6174/hw3.0/board.bin?raw=true
 wget -O /lib/firmware/ath10k/QCA6174/hw3.0/board-2.bin https://github.com/kvalo/ath10k-firmware/blob/master/QCA6174/hw3.0/board-2.bin?raw=true
 wget -O /lib/firmware/ath10k/QCA6174/hw3.0/firmware-4.bin https://github.com/kvalo/ath10k-firmware/blob/master/QCA6174/hw3.0/firmware-4.bin_WLAN.RM.2.0-00180-QCARMSWPZ-1?raw=true
